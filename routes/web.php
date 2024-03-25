d77e4dfd8728ab031cfd4446970dc5dcbacd21fe
djmzk <dimzzy24@gmail.com>
2024-03-25 21:32:54 +0700

    session login done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Events\PasswordReset;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,67 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+//bagian lupa pas
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+})->middleware('guest')->name('password.request');
+
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate(['email' => 'required|email']);
+
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+
+    return $status === Password::RESET_LINK_SENT
+                ? back()->with(['status' => __($status)])
+                : back()->withErrors(['email' => __($status)]);
+
+})->middleware('guest')->name('password.email');
+
+Route::get('/reset-password/{token}', function (string $token) {
+    return view('auth.reset-password', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
+
+
+Route::post('/reset-password', function (Request $request) {
+    $request->validate([
+        'token' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:5|confirmed',
+    ]);
+
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function ($user, $password) {
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->setRememberToken(Str::random(60));
+
+            $user->save();
+
+            event(new PasswordReset($user));
+        }
+    );
+
+    return $status === Password::PASSWORD_RESET
+                ? redirect()->route('login')->with('status', __($status))
+                : back()->withErrors(['email' => [($status)]]);
+})->middleware('guest')->name('password.update');
+// end bagian lupa pass
+
+Route::get('/daftar', [AuthController::class, 'index']);
+Route::post('/user/daftar', [AuthController::class, 'store'])->name('store');
+
+
+Route::get('/daftar', [AuthController::class, 'index']);
+Route::post('/user/daftar', [AuthController::class, 'store'])->name('store');
+
+
+Route::post('/postlogin', [AuthController::class, 'postlogin']);
+Route::get('/logout', [AuthController::class, 'logout']);
+Route::get('/login', function () {
+    return Auth::check() ? redirect('/dashboard') : view('auth.login');
+})->middleware('guest')->name('login');
+
+Route::get('/forgot/password', [AuthController::class, 'forgotPw']);
