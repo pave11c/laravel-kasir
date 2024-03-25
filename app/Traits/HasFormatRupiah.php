6e7a70325b5ebae1e3312b91243d35f1d0df9cb5
djmzk <dimzzy24@gmail.com>
2024-03-25 22:44:06 +0700

    add has rupiah

---
+++ app/Traits/HasFormatRupiah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Traits;
+
+
+trait HasFormatRupiah
+{
+    function formatRupiah($field)
+    {
+        $nominal = $this->attributes[$field];
+        return number_format($nominal, 0, ',', '.');
+    }
+}
